baf0a954ade19b68c132cf5d3f73ee540d57d077
Julian Ray <jray@camsys.com>
2015-03-14 13:16:59 -0400

    Fix comments and document paths for maintenance orders

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
   resources :maintenance_forecasts, :only => [:index]
 
   resources :maintenance_service_orders do
+    resources :comments
+    resources :documents
     member do
       get   'fire_workflow_event'
       get   'complete'
       patch  'update_service_status'
-    end    
+    end
   end
 
   resources :maintenance_providers do
@@ -17,17 +19,18 @@ Rails.application.routes.draw do
       get   'remove_asset'
     end
   end
-  
+
   resources :maintenance_schedules do
-    resources :maintenance_activities, :only => [:create, :update, :edit, :new, :destroy]    
+    resources :maintenance_activities, :only => [:create, :update, :edit, :new, :destroy]
+
     member do
       get   'add_asset'
       get   'remove_asset'
     end
   end
-  
+
   resources :inventory, :only => [], :controller => 'assets' do
-    resources :maintenance_events    
+    resources :maintenance_events
   end
-    
+
 end
