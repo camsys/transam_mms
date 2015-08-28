9ff97ca924894aa2ad3afefc548df228033db9bb
Julian Ray <jray@camsys.com>
2015-08-28 14:07:17 -0400

    Code cleanup

--- app/models/maintenance_schedule.rb
+++ app/models/maintenance_schedule.rb
@@ -12,30 +12,30 @@
 #
 #------------------------------------------------------------------------------
 class MaintenanceSchedule < ActiveRecord::Base
-    
+
   # Include the object key mixin
   include TransamObjectKey
-    
+
   #------------------------------------------------------------------------------
   # Callbacks
   #------------------------------------------------------------------------------
   after_initialize  :set_defaults
-              
+
   #------------------------------------------------------------------------------
   # Associations
   #------------------------------------------------------------------------------
   # Every maintenance schedule belongs to an organization
   belongs_to  :organization
-  
-  # Every maintenance schedule is associated with an asset subtype  
+
+  # Every maintenance schedule is associated with an asset subtype
   belongs_to  :asset_subtype
-  
+
   # Has 0 or more maintenance activities. These will be removed if the maintenance schedule is removed.
   has_many    :maintenance_activities, :dependent => :destroy
 
   # Each schedule is used by 0 or more assets
   has_and_belongs_to_many       :assets
-    
+
    # Use a nested form to set the activities
   accepts_nested_attributes_for :maintenance_activities, :allow_destroy => true
 
@@ -50,9 +50,9 @@ class MaintenanceSchedule < ActiveRecord::Base
   #------------------------------------------------------------------------------
   # Scopes
   #------------------------------------------------------------------------------
-  
-  # default scope
-  default_scope { where(:active => true) }
+
+  # Allow selection of active instances
+  scope :active, -> { where(:active => true) }
 
   # List of hash parameters allowed by the controller
   FORM_PARAMS = [
@@ -62,39 +62,39 @@ class MaintenanceSchedule < ActiveRecord::Base
     :description,
     :active,
     :maintenance_activities_attributes => [MaintenanceActivity.allowable_params]
-    
+
   ]
-  
+
   #------------------------------------------------------------------------------
   #
   # Class Methods
   #
   #------------------------------------------------------------------------------
-    
+
   def self.allowable_params
     FORM_PARAMS
   end
-  
+
   #------------------------------------------------------------------------------
   #
   # Instance Methods
   #
   #------------------------------------------------------------------------------
-      
+
   def to_s
     name
   end
-  
+
   #------------------------------------------------------------------------------
   #
   # Protected Methods
   #
   #------------------------------------------------------------------------------
-  protected 
+  protected
 
   # Set resonable defaults for a new capital project
   def set_defaults
     self.active ||= true
-  end    
-            
+  end
+
 end
