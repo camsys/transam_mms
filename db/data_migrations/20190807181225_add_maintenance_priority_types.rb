6cc1da5d692cc7b0589c20c8b7cf922838289408
mathmerized <mathmerized@gmail.com>
2019-08-27 16:45:27 -0400

    fix call to highway column in mms

--- db/data_migrations/20190807181225_add_maintenance_priority_types.rb
+++ db/data_migrations/20190807181225_add_maintenance_priority_types.rb
@@ -1,9 +1,9 @@
 class AddMaintenancePriorityTypes < ActiveRecord::DataMigration
   def up
     maintenance_priority_types = [
-        {:active => 1, :is_default => 0, :is_erf => 0, :name => 'Low',     :description => 'Lowest priority.'},
-        {:active => 1, :is_default => 1, :is_erf => 0, :name => 'Normal',  :description => 'Normal priority.'},
-        {:active => 1, :is_default => 0, :is_erf => 0, :name => 'High',    :description => 'Highest priority.'},
+        {:active => 1, :is_default => 0, :name => 'Low',     :description => 'Lowest priority.'},
+        {:active => 1, :is_default => 1, :name => 'Normal',  :description => 'Normal priority.'},
+        {:active => 1, :is_default => 0, :name => 'High',    :description => 'Highest priority.'},
     ]
 
     maintenance_priority_types.each{|m| MaintenancePriorityType.create!(m)}
